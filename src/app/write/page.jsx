e5ea5840cf32d2256abcacddb5984dfa5ef235fe
Irshad <khanirshad13113201@gmail.com>
2024-01-03 20:31:14 +0530

    updated write component

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,20 +1,21 @@
 "use client";
 import styles from "./writePage.module.css";
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation"; 
 import { useSession } from "next-auth/react";
-//import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
 import 'react-quill/dist/quill.bubble.css';
 import 'react-quill/dist/quill.snow.css';
-import ReactQuill from "react-quill";
+
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+
 const WritePage = () => {
   const { status } = useSession();
- // const ReactQuill =dynamic(()=>import('react-quill'),{ssr:false})
   const router = useRouter();
   const [value, setValue] = useState("");
   const [title, setTitle] = useState("");
 
-console.log("dec",value,"Title",title);
+  console.log("dec", value, "Title", title);
 
   if (status === "loading") {
     return <div className={styles.loading}>Loading...</div>;
@@ -31,45 +32,41 @@ console.log("dec",value,"Title",title);
       .replace(/[^\w\s-]/g, "")
       .replace(/[\s_-]+/g, "-")
       .replace(/^-+|-+$/g, "");
-      
 
   const handleSubmit = async () => {
-    
-      const res = await fetch("/api/posts", {
-        method: "POST",
-        body: JSON.stringify({
-          title,
-          desc: value,
-          slug: slugify(title),
-        }),
-      });
+    const res = await fetch("/api/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        desc: value,
+        slug: slugify(title),
+      }),
+    });
 
-      if (res.status === 200) {
-        const data = await res.json();
-        router.push(`/posts/${data.slug}`);
-      }
-    
+    if (res.status === 200) {
+      const data = await res.json();
+      router.push(`/posts/${data.slug}`);
+    }
   };
 
   return (
     <div className={styles.container}>
-      
       <input
         type="text"
         placeholder="Title"
         className={styles.input}
         onChange={(e) => setTitle(e.target.value)}
       />
-      
       <div className={styles.editor}>
-        {typeof window !== "undefined" && <ReactQuill
-          className={styles.textArea}
-          theme="snow"
-          value={value}
-          onChange={setValue}
-          placeholder="Tell your story..."
-        />}
-        
+        {typeof window !== "undefined" && (
+          <ReactQuill
+            className={styles.textArea}
+            theme="snow"
+            value={value}
+            onChange={setValue}
+            placeholder="Tell your story..."
+          />
+        )}
       </div>
       <button className={styles.publish} onClick={handleSubmit}>
         Publish
